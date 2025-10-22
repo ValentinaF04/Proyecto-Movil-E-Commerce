67fef4b406de22deedccc754ffb06a3d42e6cfc0
Valentina <va.fuenzalidab@duocuc.cl>
2025-10-22 12:18:06 +0000

    Creación de LoginScreen y lógica de AppNavigation para conectar las pantallas con las rutas

--- app/src/main/java/com/example/pcbuilder/navigation/AppNavigation.kt
+++ app/src/main/java/com/example/pcbuilder/navigation/AppNavigation.kt
@@ -1,3 +1,29 @@
 package com.example.pcbuilder.navigation
 
 //Crear el navhost y enlazar las rutas a los composables LoginScreen y RegistroScreen
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.pcbuilder.ui.screens.LoginScreen 
+import com.example.pcbuilder.ui.screens.RegistroScreen
+
+@Composable
+fun AppNavigation(){
+
+    val navController = rememberNavController()
+
+    NavHost(
+        navController = navController,
+        startDestination = AppRoutes.LOGIN_SCREEN
+    ) {
+        composable(route = AppRoutes.LOGIN_SCREEN) {
+            LoginScreen(navController = navController)
+        }
+
+        composable(route = AppRoutes.REGISTRO_SCREEN) {
+            RegistroScreen(navController = navController)
+        }
+    }
+}
