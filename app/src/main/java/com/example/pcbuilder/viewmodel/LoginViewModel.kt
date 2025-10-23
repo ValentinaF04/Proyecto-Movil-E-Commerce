aeb9613e04503d029d47d1e448927d9f2f229401
Valentina <va.fuenzalidab@duocuc.cl>
2025-10-23 15:50:02 +0000

    Creación de CartItem y CartDao

--- app/src/main/java/com/example/pcbuilder/viewmodel/LoginViewModel.kt
+++ app/src/main/java/com/example/pcbuilder/viewmodel/LoginViewModel.kt
@@ -7,7 +7,7 @@ import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
-
+import com.example.pcbuilder.data.model.User
 
 class LoginViewModel(private val userDao: UserDao) : ViewModel() {
     private val _estado = MutableStateFlow(LoginUIState())
@@ -16,17 +16,18 @@ class LoginViewModel(private val userDao: UserDao) : ViewModel() {
     fun onCorreoChange(valor: String) { _estado.update { it.copy(correo = valor) } }
     fun onClaveChange(valor: String) { _estado.update { it.copy(clave = valor) } }
 
-    fun iniciarSesion(onLoginSuccess: (isAdmin: Boolean) -> Unit) {
-        val estadoActual = _estado.value
-        viewModelScope.launch {
-            val user = userDao.getUserByEmail(estadoActual.correo)
-            if (user != null && user.password == estadoActual.clave) {
-                // Éxito
-                _estado.update { it.copy(error = null) }
-                onLoginSuccess(user.isAdmin)
-            } else {
-                // Error
-                _estado.update { it.copy(error = "Correo o contraseña inválidos") }
+    fun iniciarSesion(onLoginSuccess: (user: User?) -> Unit) {
+    val estadoActual = _estado.value
+    viewModelScope.launch {
+        val user = userDao.getUserByEmail(estadoActual.correo)
+        if (user != null && user.password == estadoActual.clave) {
+            // Éxito
+            _estado.update { it.copy(error = null) }
+            onLoginSuccess(user) // <-- 2. Devuelve el objeto 'user' completo
+        } else {
+            // Error
+            _estado.update { it.copy(error = "Correo o contraseña inválidos") }
+            onLoginSuccess(null) // <-- 3. Devuelve 'null' en caso de fallo
             }
         }
     }
